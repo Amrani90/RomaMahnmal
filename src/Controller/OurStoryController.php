25325eb22cef7f1d831b1c349aff8fa1fe1dba9c
Yousouf Am <88590585+Amrani90@users.noreply.github.com>
2024-10-07 08:25:47 +0200

    sec commit

--- src/Controller/OurStoryController.php
+++ src/Controller/OurStoryController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class OurStoryController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/our_story', name: 'app_our_story')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'Home',
+        return $this->render('our_story/index.html.twig', [
+            'controller_name' => 'OurStory',
         ]);
     }
 }
